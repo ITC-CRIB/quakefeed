b2f6cc95e531f579404131c1308ebb99fb31ede9
ali ozbakir <aozbakir@gmail.com>
2024-07-18 13:10:48 +0200

    initial commit

---
+++ src/quakefeed/__init__.py
@@ -0,0 +1,8 @@
+"""Documentation about quakefeed."""
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Ali D. Ozbakir"
+__email__ = "a.d.ozbakir@utwente.nl"
+__version__ = "0.1.0"
